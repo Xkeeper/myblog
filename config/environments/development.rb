3a39750b3196601ecc39798c8532f8785ff2d47c
Xkeeper <xkeepers@gmail.com>
2012-01-12 18:18:56 +0400

    Update gems && design work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,6 +21,7 @@ Enki::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  config.action_mailer.delivery_method = :test
 
 end
 

f9a4d72cfc0fb6f025b8630856431ba0d41a6e21
Xkeeper <xkeepers@gmail.com>
2012-04-09 17:01:05 +0400

    Added tags

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,5 +1,9 @@
 class ArchivesController < ApplicationController
   def index
     @months = Post.find_all_grouped_by_month
+    @tags = Post.tag_counts_on(:tags)
+  end
+  def tag_cloud
+    @tags = Post.tag_counts_on(:tags)
   end
 end
--- app/helpers/tag_helper.rb
+++ app/helpers/tag_helper.rb
@@ -1,4 +1,6 @@
 module TagHelper
+  include ActsAsTaggableOn::TagsHelper
+
   def linked_tag_list(tags)
     raw tags.collect {|tag| "<li>#{link_to(tag.name, posts_path(:tag => tag.name))}</li>"}.join("")
   end

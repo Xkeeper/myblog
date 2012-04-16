cd580d9c2dffef0df0b19988db97b6bf0d4e63a1
Xkeeper <xkeepers@gmail.com>
2012-04-16 15:14:39 +0400

    disable rails asset server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Enki::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
   config.assets.compress = true
   config.assets.compile = false
   config.assets.digest = true
@@ -58,5 +58,8 @@ Enki::Application.configure do
   config.time_zone = "Moscow"
 
   config.enki_config_file = Enki::Config.default
+
+  Disqus::defaults[:account] = "xkeeper"
+  Disqus::defaults[:show_powered_by] = false
 
 end
